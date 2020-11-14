f6ac162da7d75898302c03059fde9fbc5633613e
voidsatisfaction <lourie@naver.com>
2020-11-14 22:58:17 +0900

    [WIP] feat: board screen Title widget

--- lib/screen/board_screen/main.dart
+++ lib/screen/board_screen/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:snake_game/screen/board_screen/widgets/main_picture_widget.dart';
+import 'package:snake_game/screen/board_screen/widgets/title_widget.dart';
 
 class BoardScreen extends StatefulWidget {
   @override
@@ -10,6 +11,12 @@ class BoardScreen extends StatefulWidget {
 class _BoardScreenState extends State<BoardScreen> {
   final String _mainPictureURL = 'assets/images/chung_baram.jpg';
   final double _height = 210;
+
+  final String _bandTitle = '블로거 청바람 모임';
+  final bool _isPublicBand = false;
+  final int _memberCount = 33;
+  final Color _invitationColor = Colors.redAccent;
+  final Color _writeButtonColor = Colors.redAccent;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +28,13 @@ class _BoardScreenState extends State<BoardScreen> {
           MainPictureWidget(
             mainPictureUrl: _mainPictureURL,
             height: _height,
+          ),
+          TitleWidget(
+            bandTitle: _bandTitle,
+            bandCategory: _isPublicBand ? '공개' : '비공개',
+            memberCount: _memberCount,
+            writeButtonColor: _writeButtonColor,
+            invitationColor: _invitationColor,
           ),
         ],
       ),
